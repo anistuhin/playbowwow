230f35999cf51fa46b25613afd2ccbd99c3a9504
fofaliya111 <fofaliya111@gmail.com>
2020-11-11 01:46:44 -0600

    Added jquery-ui

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
 (function($) {
     $(document).ready(function() {
-        $("#age").slider({ 'tooltip': 'always' });
-        $("#distance").slider({ 'tooltip': 'always' });
+        if($("#age").length && $("#distance").length){ 
+            $("#age").slider({ 'tooltip': 'always' });
+            $("#distance").slider({ 'tooltip': 'always' });
+        }
+        if($('#datetimepicker').length){
+            $( "#datetimepicker" ).datepicker();
+        }
     });
     $(document).on('click', '#open-nav', function(e) {
         e.preventDefault();
