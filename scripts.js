0637d239e7b5054d445839a4d3044017ccdb9446
Md Anisur Rahman <anistuhin@gmail.com>
2020-11-12 09:16:02 +0600

    final revision from Anis

--- scripts.js
+++ scripts.js
@@ -4,8 +4,37 @@
             $("#age").slider({ 'tooltip': 'always' });
             $("#distance").slider({ 'tooltip': 'always' });
         }
-        if($('#datetimepicker').length){
-            $( "#datetimepicker" ).datepicker();
+        if ($('#datetimepicker').length) {
+            $("#datetimepicker").datepicker();
+        }
+        if (true) {
+            var places = [
+                "ActionScript",
+                "AppleScript",
+                "Asp",
+                "BASIC",
+                "C",
+                "C++",
+                "Clojure",
+                "COBOL",
+                "ColdFusion",
+                "Erlang",
+                "Fortran",
+                "Groovy",
+                "Haskell",
+                "Java",
+                "JavaScript",
+                "Lisp",
+                "Perl",
+                "PHP",
+                "Python",
+                "Ruby",
+                "Scala",
+                "Scheme"
+            ];
+            $("#places").autocomplete({
+                source: places
+            });
         }
     });
     $(document).on('click', '#open-nav', function(e) {
