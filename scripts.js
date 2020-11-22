515c6dcc350a33f2f67a8fb5ee33f85e87426b43
Md Anisur Rahman <anistuhin@gmail.com>
2020-11-22 23:26:53 +0600

    Notifications page done
    Added a way to clear all demo data using clear data button on the profile page

--- scripts.js
+++ scripts.js
@@ -71,6 +71,64 @@
             }
         }
 
+        if (sessionStorage.getItem("currentNotifications") == null) {
+            sessionStorage.setItem("currentNotifications", 3);
+            $('.notifications').attr('data-id', 3);
+        } else if ($('.notifications').length) {
+            $('.notifications').attr('data-id', sessionStorage.getItem("currentNotifications"));
+        }
+
+        if (sessionStorage.getItem('card1') == null) {
+            $('[data-card-id="1"]').attr('data-interacted', 'no');
+            $('[data-card-id="1"]').attr('data-status', 'na');
+        } else {
+            $('[data-card-id="1"]').attr('data-interacted', 'yes');
+            $('[data-card-id="1"]').attr('data-status', sessionStorage.getItem('card1'));
+        }
+
+        if (sessionStorage.getItem('card2') == null) {
+            $('[data-card-id="2"]').attr('data-interacted', 'no');
+        } else {
+            $('[data-card-id="2"]').attr('data-interacted', 'yes');
+        }
+
+        if (sessionStorage.getItem('card3') == null) {
+            $('[data-card-id="3"]').attr('data-interacted', 'no');
+            $('[data-card-id="3"]').attr('data-status', 'na');
+        } else {
+            $('[data-card-id="3"]').attr('data-interacted', 'yes');
+            $('[data-card-id="3"]').attr('data-status', sessionStorage.getItem('card3'));
+        }
+
+        $(document).on('click', '.btn-accept', function() {
+            $(this).closest('.card').attr('data-status', 'accepted');
+            $(this).closest('.card').attr('data-interacted', 'yes');
+            sessionStorage.setItem('card' + $(this).closest('.card').attr('data-card-id'), 'accepted');
+        });
+
+        $(document).on('click', '.btn-reject', function() {
+            $(this).closest('.card').attr('data-status', 'rejected');
+            $(this).closest('.card').attr('data-interacted', 'yes');
+            sessionStorage.setItem('card' + $(this).closest('.card').attr('data-card-id'), 'rejected');
+        });
+
+        $(document).on('click', '.btn-chat', function() {
+            $(this).closest('.card').attr('data-interacted', 'yes');
+            sessionStorage.setItem('card' + $(this).closest('.card').attr('data-card-id'), 'yes');
+        });
+
+        $(document).on('click', '.undo-status', function() {
+            $(this).closest('.card').attr('data-status', 'na');
+            sessionStorage.setItem('card' + $(this).closest('.card').attr('data-card-id'), 'na');
+        });
+
+        $(document).on('click', '[data-interacted="no"] .btn-accept, [data-interacted="no"] .btn-reject, [data-interacted="no"] .btn-chat', function() {
+            if (Number(sessionStorage.getItem("currentNotifications")) > 0) {
+                sessionStorage.setItem("currentNotifications", Number(sessionStorage.getItem("currentNotifications")) - 1);
+            }
+            $('.notifications').attr('data-id', sessionStorage.getItem("currentNotifications"));
+        });
+
         $(document).on('click', '#cancel-schedule', function() {
             $('.undo-playdate-alert').remove();
             $('main').after('<div class="undo-playdate-alert alert alert-primary alert-dismissible fade show mb-0" role="alert">The request has been unsent successfully.<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
@@ -86,6 +144,10 @@
                 size: $("#size").val()
             };
             sessionStorage.setItem("searchedData", JSON.stringify(data));
+        });
+        $(document).on('click', '#clear-profile-data', function() {
+            sessionStorage.clear();
+            location.reload();
         });
     });
 })(window.jQuery);
